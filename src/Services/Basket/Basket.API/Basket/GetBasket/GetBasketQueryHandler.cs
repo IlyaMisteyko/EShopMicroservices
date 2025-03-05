2abec96d91d99bc9df2f2bc97590b2e4e3b57b76
IlyaMisteyko <ilya.misteyko@gmail.com>
2025-03-05 23:55:51 +0100

    Implemented Endpoint and Handlers for Basket microservice, Basket repository. Injected Marten lib.

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBusketQuery(string UserName) : IQuery<GetBusketResult>;
+    public record GetBusketResult(ShoppingCart Cart);
+
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBusketQuery, GetBusketResult>
+    {
+        public async Task<GetBusketResult> Handle(GetBusketQuery query, CancellationToken cancellationToken)
+        {
+            return new GetBusketResult(new ShoppingCart("123"));
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBusket(string userName, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> StoreBusket(ShoppingCart basket, CancellationToken cancellationToken = default);
+        Task<bool> DeleteBusket(string userName, CancellationToken cancellationToken = default);
+    }
+}
