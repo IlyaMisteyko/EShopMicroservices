8262986244ae8372b5284f6486302d7a4012ac2c
IlyaMisteyko <ilya.misteyko@gmail.com>
2025-03-15 23:32:38 +0100

    Implemented DI

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,14 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddAouServices();
 
 var app = builder.Build();
 
