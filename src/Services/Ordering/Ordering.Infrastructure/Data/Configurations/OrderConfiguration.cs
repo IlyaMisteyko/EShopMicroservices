4c1a90d014ac026bc6fc6f86702601dd46dece27
IlyaMisteyko <ilya.misteyko@gmail.com>
2025-03-18 00:05:22 +0100

    Revert "Updated model configurations"
    This reverts commit 22d9fb07fcf0e361a3f14ba45fc693ac616bf7bc.

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Enums;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
 
 namespace Ordering.Infrastructure.Data.Configurations
 {
@@ -7,114 +8,7 @@ namespace Ordering.Infrastructure.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<Order> builder)
         {
-            builder.HasKey(o => o.Id);
-
-            builder.Property(o => o.Id)
-                .HasConversion(
-                orderId => orderId.Value,
-                dbId => OrderId.Of(dbId));
-
-            builder.HasOne<Customer>()
-                .WithMany()
-                .HasForeignKey(o => o.CustomerId)
-                .IsRequired();
-
-            builder.HasMany(o => o.OrderItems)
-                .WithOne()
-                .HasForeignKey(oi => oi.OrderId);
-
-            builder.ComplexProperty(
-                o => o.OrderName, nameBuilder =>
-                {
-                    nameBuilder.Property(n => n.Value)
-                    .HasColumnName(nameof(Order.OrderName))
-                    .HasMaxLength(100)
-                    .IsRequired();
-                });
-
-            builder.ComplexProperty(
-                o => o.ShippingAddress, saBuilder =>
-                {
-                    saBuilder.Property(sa => sa.FirstName)
-                    .HasMaxLength(50)
-                    .IsRequired();
-
-                    saBuilder.Property(sa => sa.LastName)
-                    .HasMaxLength(50)
-                    .IsRequired();
-
-                    saBuilder.Property(sa => sa.EmailAddress)
-                    .HasMaxLength(50);
-
-                    saBuilder.Property(sa => sa.AddressLine)
-                    .HasMaxLength(180)
-                    .IsRequired();
-
-                    saBuilder.Property(sa => sa.Country)
-                    .HasMaxLength(50);
-
-                    saBuilder.Property(sa => sa.State)
-                    .HasMaxLength(50);
-
-                    saBuilder.Property(sa => sa.ZipCode)
-                    .HasMaxLength(5)
-                    .IsRequired();
-                });
-
-            builder.ComplexProperty(
-                o => o.BillingAddress, baBuilder =>
-                {
-                    baBuilder.Property(ba => ba.FirstName)
-                    .HasMaxLength(50)
-                    .IsRequired();
-
-                    baBuilder.Property(sa => sa.LastName)
-                    .HasMaxLength(50)
-                    .IsRequired();
-
-                    baBuilder.Property(sa => sa.EmailAddress)
-                    .HasMaxLength(50);
-
-                    baBuilder.Property(sa => sa.AddressLine)
-                    .HasMaxLength(180)
-                    .IsRequired();
-
-                    baBuilder.Property(sa => sa.Country)
-                    .HasMaxLength(50);
-
-                    baBuilder.Property(sa => sa.State)
-                    .HasMaxLength(50);
-
-                    baBuilder.Property(sa => sa.ZipCode)
-                    .HasMaxLength(5)
-                    .IsRequired();
-                });
-
-            builder.ComplexProperty(
-                o => o.Payment, paymentBuilder =>
-                {
-                    paymentBuilder.Property(p => p.CardNumber)
-                    .HasMaxLength(50);
-
-                    paymentBuilder.Property(p => p.CardNumber)
-                    .HasMaxLength(24)
-                    .IsRequired();
-
-                    paymentBuilder.Property(p => p.Expiration)
-                    .HasMaxLength(10);
-
-                    paymentBuilder.Property(p => p.Expiration)
-                    .HasMaxLength(3);
-
-                    paymentBuilder.Property(p => p.PaymentMethod);
-                });
-
-            builder.Property(o => o.Status)
-                .HasDefaultValue(OrderStatus.Draft)
-                .HasConversion(s => s.ToString(),
-                dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus));
-
-            builder.Property(o => o.TotalPrice);
+            throw new NotImplementedException();
         }
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
 
 namespace Ordering.Infrastructure.Data.Configurations
 {
@@ -6,20 +8,7 @@ namespace Ordering.Infrastructure.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<OrderItem> builder)
         {
-            builder.HasKey(oi => oi.Id);
-
-            builder.Property(oi => oi.Id)
-                .HasConversion(
-                orderItemId => orderItemId.Value,
-                dbId => OrderItemId.Of(dbId));
-
-            builder.HasOne<Product>()
-                .WithMany()
-                .HasForeignKey(oi => oi.ProductId);
-
-            builder.Property(oi => oi.Quantity).IsRequired();
-
-            builder.Property(oi => oi.Price).IsRequired();
+            throw new NotImplementedException();
         }
     }
 }
