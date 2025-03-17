22d9fb07fcf0e361a3f14ba45fc693ac616bf7bc
IlyaMisteyko <ilya.misteyko@gmail.com>
2025-03-18 00:03:39 +0100

    Updated model configurations

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Models;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Ordering.Infrastructure.Data.Configurations
 {
@@ -8,7 +6,20 @@ namespace Ordering.Infrastructure.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<OrderItem> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(oi => oi.Id);
+
+            builder.Property(oi => oi.Id)
+                .HasConversion(
+                orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(oi => oi.ProductId);
+
+            builder.Property(oi => oi.Quantity).IsRequired();
+
+            builder.Property(oi => oi.Price).IsRequired();
         }
     }
 }
